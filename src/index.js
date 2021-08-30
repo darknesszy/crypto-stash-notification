03a3b37bacce88cba73d0172c0259f69cea8c23a
Michael <25147960+darknesszy@user.noreply.github.com>
2021-08-30 10:36:26 +0800

    :bug: Made conversions clearer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { notify$ } from './push-notification'
 import { switchMap } from 'rxjs/operators'
 import { currentTotal, statusMessage } from './evaluation'
 import { merge, of } from 'rxjs'
-import { minutesToEpoch } from './utils'
+import { hashesToGHash, mHashToHashes, minutesToEpoch } from './utils'
 
 // Setup polyfill for rxjs fromFetch
 global.fetch = fetch
@@ -16,6 +16,7 @@ global.AbortController = AbortController
 !dotenv.config().error && console.log('Using Environment Variables from .env file...')
 
 const gracePeriod = 60 // 1 hour
+const expectedTotal = mHashToHashes(Number(process.env['EXPECTED_TOTAL']))
 let lastAlert = undefined
 
 const createReport = status => merge(
@@ -26,15 +27,15 @@ const createReport = status => merge(
 statusUpdate$(process.env['API_SERVER'], 60000)
     .pipe(
         switchMap(status => {
-            if(currentTotal < Number(process.env['EXPECTED_TOTAL'])) {
+            if(currentTotal < expectedTotal) {
                 if(lastAlert == undefined) {
                     lastAlert = new Date().getTime()
                     return merge(
-                        notify$(4, `Hashrate has dropped to ${(currentTotal / 1000000000).toFixed(3)} GH/s`, process.env['API_SERVER']),
+                        notify$(4, `Hashrate has dropped to ${hashesToGHash(currentTotal).toFixed(3)} GH/s`, process.env['API_SERVER']),
                         createReport(status)
                     )
                 } else if(new Date().getTime() - lastAlert > minutesToEpoch(gracePeriod)) {
-                    return notify$(4, `Hashrate is still abnormal at ${(currentTotal / 1000000000).toFixed(3)} GH/s`, process.env['API_SERVER'])
+                    return notify$(4, `Hashrate is still abnormal at ${hashesToGHash(currentTotal).toFixed(3)} GH/s`, process.env['API_SERVER'])
                 }
             } else {
                 lastAlert = undefined
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
 export const minutesToEpoch = min => min * 60 * 1000
 
-export const EpochToMinutes = epoch => epoch / 1000 / 60
+export const epochToMinutes = epoch => epoch / 1000 / 60
+
+export const mHashToHashes = mHash => mHash * 1000000
+
+export const hashesToMHash = hashes => hashes / 1000000
+
+export const hashesToGHash = hashes => hashes / 1000000000
